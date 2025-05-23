dd85130a851d56a2167aa219d7384b4e7aa49778
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-05-23 10:35:32 -0700

    .gen

--- generate.bash
+++ generate.bash
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-cargo generate --init --overwrite --allow-commands --path ../templates www/site/product/info --name filer-sourcetrait-com --values-file ./cargo-generate.values.toml
+.gen/generate.bash
